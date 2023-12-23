ade2252462b881e20576f15c128028f31299ac67
GGGG <kudashovda@yandex.ru>
2023-12-23 19:54:10 +0300

    Задание 6

--- src/com/geekbrains/task5/Task5.java
+++ src/com/geekbrains/task5/Task5.java
@@ -14,48 +14,52 @@ public class Task5 {
 
         public static void main(String[] args) throws InterruptedException {
             int count = 0;
-            int k = 3;
-            int[][] matrix1 = new int[k][k];
-            int[][] matrix2 = new int[k][k];
-            int[][] matrix2T = new int[k][k];
+            int k11 = 3;
+            int k12 = 3;
+            int k21 = 3;
+            int k22 = 3;
+            int[][] matrix1 = new int[k11][k12];
+            int[][] matrix2 = new int[k12][k22];
+            int[][] matrix2T = new int[k12][k22];
 
-            for(int i = 0; i < matrix1.length; i++) {
-                for(int j = 0; j < matrix1.length; j++) {
-                    matrix1[i][j] = count;
+
+            for(int i = 0; i < k11; i++) {
+                for(int j = 0; j < k12; j++) {
+                    matrix1[i][j] = count + ((int) (Math.random() * 9));
                     count++;
                 }
             }
             PrintMat(matrix1);
 
-            for(int i = 0; i < matrix2.length; i++) {
-                for(int j = 0; j < matrix2.length; j++) {
-                    matrix2[i][j] = count;
+            for(int i = 0; i < k21; i++) {
+                for(int j = 0; j < k22; j++) {
+                    matrix2[i][j] = count+ ((int) (Math.random() * 9));
                     count++;
                 }
             }
             PrintMat(matrix2);
 
-            for(int i = 0; i < matrix2T.length; i++) {
-                for(int j = 0; j < matrix2T.length; j++) {
+            for(int i = 0; i < k21; i++) {
+                for(int j = 0; j < k22; j++) {
                     matrix2T[i][j] = matrix2[j][i];
                 }
             }
             //PrintMat(matrix2T);
 
-            multiply[][] thread = new multiply[3][3];
-            for(int i = 0; i < matrix1.length; i++) {
-                for(int j = 0; j < matrix1.length; j++) {
+            multiply[][] thread = new multiply[k12][k22];
+            for(int i = 0; i < k11; i++) {
+                for(int j = 0; j < k22; j++) {
                     thread[i][j] = new multiply(matrix1[i],matrix2T[j]);
                     thread[i][j].start();
                 }
             }
-            for(int i = 0; i < matrix1.length; i++) {
-                for(int j = 0; j < matrix2.length; j++) {
+            for(int i = 0; i < k11; i++) {
+                for(int j = 0; j < k22; j++) {
                     thread[i][j].join();//ожиданет конца другого потока
                 }
             }
-            for(int i = 0; i < matrix1.length; i++) {
-                for(int j = 0; j < matrix1.length; j++) {
+            for(int i = 0; i < k11; i++) {
+                for(int j = 0; j < k22; j++) {
                     System.out.print(thread[i][j].getResult() + " ");
                 }
                 System.out.println("");
@@ -76,13 +80,18 @@ public class Task5 {
             @Override
             public void run() {
                 int temp = 0;
-                for (int k = 0; k < 3; k++) {
+                for (int k = 0; k < row.length; k++) {
                     temp += row[k] * column[k];
                 }
                 result = temp;
             }
         }
 }
+
+
+
+
+
 
 
 
